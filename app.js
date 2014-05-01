26382f38f63c1d1c31eb9451571e892f54d18b32
madhavgoel <madhavgoel@berkeley.edu>
2014-04-30 19:25:03 -0700

    COMPLETE

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var ejs = require("ejs");
 var app = express();
-
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('facts.db');
 
 ///////////////////////////////////////////////////////////////////////////////
 // APP CONFIGURATION                                                         //
@@ -48,20 +49,32 @@ app.get('/chain-reaction', function(req, res) {
   res.render('chain-reaction.html', { });
 });
 
-var faa = ["Every US president has worn glasses (just not always in public).", "Salt Lake City has a law against carrying an unwrapped ukulele on the street.", "A snail can have 25,000 teeth.", "No matter where you stand in Michigan, you are never more than 85 miles from a Great Lake.", "No matter where you stand in Michigan, you are never more than 85 miles from a Great Lake.", "It is physically impossible for pigs to look up in the sky."];
+//var faa = ["Every US president has worn glasses (just not always in public).", "Salt Lake City has a law against carrying an unwrapped ukulele on the street.", "A snail can have 25,000 teeth.", "No matter where you stand in Michigan, you are never more than 85 miles from a Great Lake.", "No matter where you stand in Michigan, you are never more than 85 miles from a Great Lake.", "It is physically impossible for pigs to look up in the sky."];
 
-app.get('/fact', function(req, res) {
-  res.render('fact.html', {f: faa[Math.floor(Math.random() * 6)]
+app.get('/fact', function(req, res) {  
+    db.get('SELECT * FROM fact_table ORDER BY RANDOM()', function(err, item) {
+      res.render('fact.html', {f: item.fact_str});  //('fact.html', {f: faa[Math.floor(Math.random() * 6)]});
     });
 });
 app.get('/facts', function(req, res) {
+  db.all('SELECT * FROM fact_table', function(err, items) {
+  var faa = [];
+
+  for (var i = 0; i < items.length; i++) {
+    faa.push(items[i].fact_str);
+  };
+
   res.render('facts.html', {faa: faa});
+  
+  });
 });
 
 app.get('/submit_fact', function(req, res){
   var inputtext = req.query['fact'];
+  db.run('INSERT INTO fact_table VALUES (" '+ inputtext +'")');
   faa.push(inputtext);
-  res.redirect('/facts');
+  res.redirect('/fact');
+  
 });
 
 ///////////////////////////////////////////////////////////////////////////////
